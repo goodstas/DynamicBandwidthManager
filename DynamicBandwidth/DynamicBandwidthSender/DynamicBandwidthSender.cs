44e4731df3b980aab5069a424b77af3d73bd9666
aven1812 <aven1812@ENG10044845>
2023-03-22 16:38:08 +0200

    added comments

--- DynamicBandwidth/DynamicBandwidthSender/DynamicBandwidthSender.cs
+++ DynamicBandwidth/DynamicBandwidthSender/DynamicBandwidthSender.cs
@@ -9,25 +9,17 @@ namespace DynamicBandwidthSender;
 
 public class DynamicBandwidthSender : BackgroundService
 {
-    private static ConnectionMultiplexer? _connection;
-
-    private static readonly string[] _dataTypes = {"Track", "Plot", "Sensor Status", "Mission Status"};
-
-    private static readonly MessageMetric Total = new();
-    private static readonly Dictionary<string,MessageMetric> MessageMetrics = new();
-
-    private readonly string _chunkChannel = "chunk-channel";
-    private readonly ILogger<DynamicBandwidthSender> _logger;
-    private readonly RedisConnectionProvider _provider;
-
+    #region Constructor
 
     public DynamicBandwidthSender(ILogger<DynamicBandwidthSender> logger, IConfiguration config)
     {
         _logger = logger;
 
+        // get connection string for Redis
         var connectionString = config.GetConnectionString("REDIS_CONNECTION_STRING");
         try
         {
+            // connect to Redis
             _connection = ConnectionMultiplexer.Connect(connectionString);
         }
         catch (Exception e)
@@ -35,69 +27,50 @@ public class DynamicBandwidthSender : BackgroundService
             _logger.Log(LogLevel.Error, e.Message);
         }
 
+        // initialize redis provider
         _provider = new($"redis://{connectionString}");
 
-        foreach (var dataType in _dataTypes)
-        {
-            MessageMetrics.Add(dataType,new(dataType));
-        }
+        // initialize metrics
+        foreach (var dataType in DataTypes) MessageMetrics.Add(dataType, new(dataType));
     }
 
+    #endregion
 
-    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    #region Message Metric
+
+    // a class to hold Messages Metrics for Prometheus
+    private class MessageMetric
     {
-        if (_connection != null)
-        {
-            var subscriber = _connection.GetSubscriber();
-            await subscriber.SubscribeAsync(_chunkChannel, SendMessages);
-        }
-    }
+        #region Fields
+        // number of messages of current type in current chunk
+        private readonly Gauge _count;
 
-    private async void SendMessages(RedisChannel channel, RedisValue json)
-    {
-        var chunk = JsonSerializer.Deserialize<Chunk>(json);
-        if (chunk == null) return;
+        // total number of bytes sent of current type in current chunk
+        private readonly Gauge _size;
+        #endregion
 
-        var ids = chunk.MessagesIds.Select(id => $"{nameof(Message)}:{id}");
+        #region Constructor
 
-        var messages = await _provider.RedisCollection<Message>().FindByIdsAsync(ids);
-
-        foreach (var message in messages)
-            if (message.Value != null)
-                _logger.Log(LogLevel.Debug,
-                    $"Send message: {message.Value.DataType}, Size: {message.Value.Data.Length} bytes");
-
-        // create metrics and view in grafana
-
-        Total.Set(chunk.Count,chunk.Size);
-
-        foreach (var dataType in chunk.MessagesStatistics.Keys.Where(dataType => MessageMetrics.ContainsKey(dataType)))
-        {
-            MessageMetrics[dataType].Set(chunk.MessagesStatistics[dataType]);
-        }
-    }
-
-    internal class MessageMetric
-    {
+        // default constructor for Total Metric
         public MessageMetric()
         {
             _size = Metrics.CreateGauge("total_messages_size", "Number of bytes that has been sent");
             _count = Metrics.CreateGauge("total_messages_count", "Number of messages that has been sent");
-        }
+        } 
 
-
+        //constructor for specific Data Type
         internal MessageMetric(string Type)
         {
-            var lower = Type.ToLower().Replace(" ","_");
+            var lower = Type.ToLower().Replace(" ", "_");
             _size = Metrics.CreateGauge($"{lower}_messages_size",
                 $"Number of bytes that has been sent for type {Type}");
             _count = Metrics.CreateGauge($"{lower}_messages_count",
                 $"Number of messages that has been sent for type {Type}");
         }
+        #endregion
 
-        private readonly Gauge _size;
-        private readonly Gauge _count;
-
+        #region Methods
+        // set gauges values
         public void Set(DataStatistics statistics)
         {
             _size.Set(statistics.Size);
@@ -107,6 +80,95 @@ public class DynamicBandwidthSender : BackgroundService
         public void Set(int count, int size)
         {
             _size.Set(size);
-            _count.Set(count);        }
+            _count.Set(count);
+        } 
+        #endregion
     }
+
+    #endregion
+
+    #region Fields
+
+    //types of messages
+    private static readonly string[] DataTypes = {"Track", "Plot", "Sensor Status", "Mission Status"};
+
+    // metrics to display with Prometheus and Grafana
+    private static readonly MessageMetric Total = new();
+    private static readonly Dictionary<string, MessageMetric> MessageMetrics = new();
+
+    // channel to subscribe to Redis in order to get chunks
+    private const string ChunkChannel = "chunk-channel";
+
+    // connection handler to Redis
+    private static ConnectionMultiplexer? _connection;
+
+    // logger 
+    private readonly ILogger<DynamicBandwidthSender> _logger;
+
+    // an instance of Redis Provider for fetching messages from redis
+    private readonly RedisConnectionProvider _provider;
+
+    // cancellation token for exit
+    private CancellationToken _cancel;
+
+    // an instance of Redis Subscriber for subscribe to get chunks
+    private ISubscriber? _subscriber;
+
+    #endregion
+
+    #region Methods
+
+    /// <summary>
+    ///     main function of the service
+    /// </summary>
+    /// <param name="stoppingToken">stop when set to cancel</param>
+    /// <returns></returns>
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        _cancel = stoppingToken;
+
+        if (_connection != null)
+        {
+            // subscribe to Redis for getting chunks
+            _subscriber = _connection.GetSubscriber();
+            await _subscriber.SubscribeAsync(ChunkChannel, SendMessages);
+        }
+    }
+
+    /// <summary>
+    ///     received chunk from Manger, send all the messages that are in it
+    /// </summary>
+    /// <param name="channel"></param>
+    /// <param name="json"></param>
+    private async void SendMessages(RedisChannel channel, RedisValue json)
+    {
+        // got cancelled
+        if (_cancel.IsCancellationRequested)
+        {
+            // unsubscribe from Redis
+            if (_subscriber != null) await _subscriber.UnsubscribeAsync(ChunkChannel);
+            return;
+        }
+
+        // parse the chunk
+        var chunk = JsonSerializer.Deserialize<Chunk>(json);
+        if (chunk == null) return;
+
+        // fetch the messages
+        var ids = chunk.MessagesIds.Select(id => $"{nameof(Message)}:{id}");
+        var messages = await _provider.RedisCollection<Message>().FindByIdsAsync(ids);
+
+        // it's a demo - just log messages types and sizes
+        foreach (var message in messages)
+            if (message.Value != null)
+                _logger.Log(LogLevel.Debug,
+                    $"Send message: {message.Value.DataType}, Size: {message.Value.Data.Length} bytes");
+
+        // set metrics
+        Total.Set(chunk.Count, chunk.Size);
+        foreach (var dataType in chunk.MessagesStatistics.Keys.Where(dataType => MessageMetrics.ContainsKey(dataType)))
+            MessageMetrics[dataType].Set(chunk.MessagesStatistics[dataType]);
+    }
+
+    #endregion
 }
