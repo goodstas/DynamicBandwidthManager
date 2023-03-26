f54c5103d1d8acc7f75401ff1777d2d68ea066fb
davidamor77 <dav.amor.77@gmail.com>
2023-03-26 06:33:10 +0000

    environment vriable

--- DynamicBandwidth/DataHandlerApp/Program.cs
+++ DynamicBandwidth/DataHandlerApp/Program.cs
@@ -6,8 +6,14 @@ using System.Runtime.CompilerServices;
 using static System.Net.WebRequestMethods;
 
 var builder = WebApplication.CreateBuilder(args);
-string port1 = "http://localhost:500" + args[0];
-int suffixPortNumber = (Int32.Parse(args[0]) + 100);
+
+builder.Configuration.AddEnvironmentVariables();
+
+
+int runningChannelID = builder.Configuration.GetValue<int>("RunningChannelID");
+
+string port1 = "http://localhost:500" + runningChannelID;
+int suffixPortNumber = runningChannelID + 100;
 string port2 = "http://localhost:5" + suffixPortNumber.ToString();
 builder.WebHost.UseUrls(port1, port2);
 
@@ -22,7 +28,7 @@ builder.Services.AddSingleton(redisProvider);
 
 DataHandlerConfig dataHandlerConfig = builder.Configuration.GetSection("DataHandlerConfig")
                                                 .Get<DataHandlerConfig>();
-dataHandlerConfig.RunningChannelID = Int32.Parse(args[0]);
+
 builder.Services.AddSingleton(dataHandlerConfig);
 
 var app = builder.Build();
